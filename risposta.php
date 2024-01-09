5cbfb59902f8948e73f8f850c0e52eb3b77e2962
AndreaGul <andreagulino99@gmail.com>
2024-01-09 10:32:55 +0100

    feat: formattazione testo della risposta

--- risposta.php
+++ risposta.php
@@ -18,15 +18,15 @@
   <div>
   <h2>Paragrafo</h2>
   <p> <?php echo $parag; ?> </p>
-  <h4>Lunghezza del paragrafo</h4>
-  <p> <?php echo strlen($parag); ?></p>
+  <h4>La lunghezza del paragrafo è di <?php echo strlen($parag);?> caratteri</h4>
+  
 
   <h2>Parola da censurare</h2>
   <p> <?php echo $word; ?></p>
   <h2>Paragrafo censurato</h2>
   <p> <?php echo $cens_paragraf; ?></p>
-  <h4>lunghezza paragrafo censurato</h4>
-  <p> <?php echo strlen($cens_paragraf); ?></p>
+  <h4>La lunghezza del paragrafo censurato è di <?php echo strlen($cens_paragraf);?> caratteri</h4>
+  
 
 </div>
 </body>
